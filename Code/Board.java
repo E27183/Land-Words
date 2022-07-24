dc81832582c1a9bd3a42592afa29249eb1964f59
Edan Landow <edan.landow@gmail.com>
2022-07-24 18:44:18 +1000

    Board printout progress

--- Code/Board.java
+++ Code/Board.java
@@ -1,4 +1,5 @@
 import java.util.Random;
+import java.lang.StringBuilder;
 
 public class Board {
   char[][] board = new char[13][20];
@@ -9,16 +10,18 @@ public class Board {
   boolean left_player_turn;
   boolean start_of_game;
   boolean last_turn_skipped;
+  boolean end_of_game;
   int[] character_counts = {9, 2, 2, 4, 12, 2, 3, 2, 9, 1, 1, 4, 2, 6, 8, 2, 1, 6, 4, 6, 4, 2, 2, 1, 2, 1};
 
   public Board() {
     this.start_of_game = true;
+    this.end_of_game = false;
     this.last_turn_skipped = false;
     Random r = new Random();
     this.left_player_turn = r.nextBoolean();
     for (int i = 0; i < 13; i++) {
       for (int j = 0; j < 20; j++) {
-        this.board[i][j] = '0';
+        this.board[i][j] = ' ';
         this.ownership[i][j] = '0';
       }
     }
@@ -40,4 +43,45 @@ public class Board {
     }
     this.position_in_bag = 7;
   }
+
+  @Override
+  public String toString() {
+    StringBuilder builder = new StringBuilder();
+    if (!end_of_game) {
+      if (left_player_turn) {
+        builder.append("Left");
+      } else {
+        builder.append("Right");
+      }
+      builder.append(" player to play.\nAvailable letters: ");
+      for (int i = 0; i < 7; i++) {
+        builder.append(this.pieces[i]);
+        builder.append("  ");
+      }
+      builder.append("\nLetters remaining: ");
+      builder.append(98 - this.position_in_bag);
+      builder.append("\n");
+    } else {
+      builder.append("Game is over.\n");
+    }
+    builder.append("Score: ");
+    //TODO
+    builder.append("\n\n   ");
+    for (int i = 0; i < 20; i++) {
+      builder.append(" ");
+      builder.append((char) (97 + i));
+    }
+    for (int i = 0; i < 13; i++) {
+      builder.append("\n   +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+\n");
+      builder.append((char) (97 + i));
+      builder.append("  |");
+      for (int j = 0; j < 20; j++) {
+        builder.append(this.board[i][j]);
+        builder.append("|");
+      }
+    }
+    builder.append("\n   +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+\n");
+    return builder.toString();
+  }
+
 }
--- Code/IO.java
+++ Code/IO.java
@@ -1,5 +1,6 @@
 public class IO {
   public static void main(String[] args) {
-    System.out.println("yea");
+    Board b = new Board();
+    System.out.println(b);
   }
 }
